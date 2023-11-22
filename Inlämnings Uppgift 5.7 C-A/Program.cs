204d013a140c9b7ed098f21957d9006c4a623e31
sere06001 <sere06001@utb.vaxjo.se>
2023-11-22 18:46:07 +0100

    Add project files.

---
+++ Inlämnings Uppgift 5.7 C-A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift5_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] städer = { "1. Kina", "2. Indien", "3. USA", "4. Indonesien", "5. Pakistan" };
+            for (int i = 0; i < städer.Length; i++)
+            {
+                Console.WriteLine(städer[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Mata in dem länder du vill ska visas igen genom att skriva deras nummer separerat av mellanslag.");
+            string[] svar = Console.ReadLine().Split();
+            Console.WriteLine();
+            for (int i = 0; i < svar.Length; i++)
+            {
+                int ny = int.Parse(svar[i]);
+                Console.WriteLine(städer[ny-1]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
